d1f7f9f0b6b6b17bde3ab166d3776687c42c06ec
Erik Pihl <100573213+ErikPihl@users.noreply.github.com>
2023-11-05 21:23:31 +0100

    Corrected mistake

--- 2023-10-13 - Neurala nätverk i mjukvara (del IV)/neural_network/src/dense_layer.cpp
+++ 2023-10-13 - Neurala nätverk i mjukvara (del IV)/neural_network/src/dense_layer.cpp
@@ -1,35 +1,41 @@
-#include <dense_layer.hpp>
+#include "dense_layer.hpp"
 
 namespace yrgo {
 namespace machine_learning {
 
 namespace {
 double GetActFuncOutput(const double sum, const enum ActFunc act_func) {
-    return act_func == ActFunc::kRelu ? 
+    return act_func == ActFunc::kRelu ?
         utils::math::Relu(sum) : utils::math::Tanh(sum);
 }
 
 double GetActFuncDelta(const double output, const enum ActFunc act_func) {
-    return act_func == ActFunc::kRelu ? 
+    return act_func == ActFunc::kRelu ?
         utils::math::ReluDelta(output) : utils::math::TanhDelta(output);
 }
 
 } /* namespace */
 
-DenseLayer::DenseLayer(const std::size_t num_nodes, 
-                       const std::size_t num_weights_per_node,
-                       const enum ActFunc act_func) {
+DenseLayer::DenseLayer(const std::size_t num_nodes,
+    const std::size_t num_weights_per_node,
+    const enum ActFunc act_func) {
+    Init(num_nodes, num_weights_per_node, act_func);
+}
+
+void DenseLayer::Init(const std::size_t num_nodes,
+    const std::size_t num_weights_per_node,
+    const enum ActFunc act_func) {
     utils::random::Init();
-    output_.resize(num_nodes, 0); 
+    output_.resize(num_nodes, 0);
     utils::random::InitVector<double>(bias_, num_nodes, 0, 1);
     error_.resize(num_nodes, 0);
     utils::random::InitVector<double>(weights_, num_nodes, num_weights_per_node, 0, 1);
     act_func_ = act_func;
-} 
+}
 
 void DenseLayer::Feedforward(const std::vector<double>& inputs) {
     for (std::size_t i{}; i < NumNodes(); ++i) {
-        double sum{bias_[i]};
+        double sum{ bias_[i] };
         for (std::size_t j{}; j < NumWeightsPerNode() && j < inputs.size(); ++j) {
             sum += inputs[j] * weights_[i][j];
         }
@@ -38,7 +44,7 @@ void DenseLayer::Feedforward(const std::vector<double>& inputs) {
 }
 
 void DenseLayer::Backpropagate(const std::vector<double>& reference) {
-    for (std::size_t i{}; i < NumNodes(); ++i) {
+    for (std::size_t i{}; i < NumNodes() && i < reference.size(); ++i) {
         const double error = reference[i] - output_[i];
         error_[i] = error * GetActFuncDelta(output_[i], act_func_);
     }
@@ -48,7 +54,7 @@ void DenseLayer::Backpropagate(const DenseLayer& next_layer) {
     for (std::size_t i{}; i < NumNodes(); ++i) {
         double error{};
         for (std::size_t j{}; j < next_layer.NumNodes(); ++j) {
-            error += next_layer.error_[j] * weights_[j][i];
+            error += next_layer.error_[j] * next_layer.weights_[j][i];
         }
         error_[i] = error * GetActFuncDelta(output_[i], act_func_);
     }

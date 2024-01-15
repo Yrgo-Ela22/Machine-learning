7e8e780b1ad39b001c17a1a265a2db3c6f3a599e
Erik Pihl <erik_pihl_91@hotmail.com>
2024-01-15 10:25:51 +0100

    Change number of epochs during training

--- neural_network_python/main.py
+++ neural_network_python/main.py
@@ -5,9 +5,9 @@ from utils import ActFunc
 def main() -> None:
     trainInput = ((0, 0), (0, 1), (1, 0), (1, 1))
     trainOutput = ((0), (1), (1), (0))
-    neuralNetwork = NeuralNetwork(2, 2, 1, ActFunc.Tanh)
+    neuralNetwork = NeuralNetwork(2, 3, 1, ActFunc.Tanh)
     neuralNetwork.addTrainingData(trainInput, trainOutput)
-    if neuralNetwork.train(1000, 0.1):
+    if neuralNetwork.train(200000, 0.1):
         neuralNetwork.printPredictions(trainInput)
 
 
